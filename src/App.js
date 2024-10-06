0f80e1ebd371c4c6c8f05fecf65d7e006f300822
enzo-lopez <enzofabianlopez96@gmail.com>
2024-10-06 02:36:36 -0300

    Busqueda por nombre agregada

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       background: 'linear-gradient(to bottom, #000 0%, #141414 100%)',
       minHeight: '100vh'
     }}>
-      <header className="App-header">
-        <h1>Netflix Grupo 1</h1>
-      </header>
+      
       <main>
         <MovieList />
       </main>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MovieCard from './MovieCard';
 import MovieDetails from './MovieDetails';
 import moviesData from '../data/movies.json';
 
 function MovieList() {
   const [movies, setMovies] = useState([]); // Todas las películas
+  const [moviesConFiltro, setMoviesConFiltro] = useState([]) // P. filtradas por titulo
+  const [tituloBuscado, setTituloBuscado] = useState('') // Pelicula a buscar
+
   const [selectedMovie, setSelectedMovie] = useState(null); // Película seleccionada
   const [randomMovies, setRandomMovies] = useState([]); // Películas aleatorias
-
   const scrollRefs = useRef([]); // ref para cada categoría -> usamos para el scroll
 
   const getRandomMovies = (movies, count) => {
@@ -21,12 +23,29 @@ function MovieList() {
   }, []); // Este efecto se ejecuta solo una vez cuando la página se carga
 
   const handleMovieClick = (movie) => {
-    setSelectedMovie(movie); 
+    setSelectedMovie(movie);
   };
 
   const handleCloseDetails = () => {
-    setSelectedMovie(null); 
+    setSelectedMovie(null);
   };
+
+  const buscarPelicula = ({ tituloBuscado }) => {
+    return movies.filter(movie =>
+      movie.titulo.toLowerCase().includes(tituloBuscado.toLowerCase()));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(tituloBuscado !== '')
+    setMoviesConFiltro(buscarPelicula({ tituloBuscado }))
+  }
+
+  const handleChange = (event) => {
+    const search = event.target.value
+    if (search === '') setMoviesConFiltro('')
+    setTituloBuscado(search)
+  }
 
   const scrollLeft = (index) => {
     if (scrollRefs.current[index]) {
@@ -40,32 +59,70 @@ function MovieList() {
     }
   };
 
+  const busqueda = [
+    { name: "Tendencias", movies: moviesConFiltro }
+  ]
+
   const categories = [
     { name: "Tendencias", movies: movies.slice(0, 12) },
     { name: "Populares en Netflix", movies: movies.slice(8, 20) },
     { name: "Volver a ver", movies: movies.slice(16, 20) },
-    { name: "Slide Aleatorio", movies: randomMovies }
+    { name: "Slide Aleatorio", movies: randomMovies },
+    { name: "Mi lista", movies: [] }
   ];
 
   return (
     <div>
-      {categories.map((category, index) => (
-        <div key={index}>
-          <h2 style={{ marginLeft: '4rem', color: '#e5e5e5' }}>{category.name}</h2>
-          
-          <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
-            <button onClick={() => scrollLeft(index)} className="scroll-button">◀</button>
 
-            <section className="movie-list" ref={el => scrollRefs.current[index] = el}>
-              {category.movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} onClick={handleMovieClick} />
-              ))}
-            </section>
+      <header className="App-header">
+        <h1>Netflix Grupo 1</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={tituloBuscado}
+            placeholder="Coco, Forrest Gump, Inception.." />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
 
-            <button onClick={() => scrollRight(index)} className="scroll-button">▶</button>
-          </div>
-        </div>
-      ))}
+      {
+        moviesConFiltro !== '' ? (
+          busqueda.map((category, index) => (
+            <div key={index}>
+              <h2 style={{ marginLeft: '4rem', color: '#e5e5e5' }}>Resultados</h2>
+
+              <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                <button onClick={() => scrollLeft(index)} className="scroll-button">◀</button>
+
+                <section className="movie-list" ref={el => scrollRefs.current[index] = el}>
+                  {category.movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} onClick={handleMovieClick} />
+                  ))}
+                </section>
+
+                <button onClick={() => scrollRight(index)} className="scroll-button">▶</button>
+              </div>
+            </div>
+          ))
+        ) : (
+          categories.map((category, index) => (
+            <div key={index}>
+              <h2 style={{ marginLeft: '4rem', color: '#e5e5e5' }}>{category.name}</h2>
+
+              <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                <button onClick={() => scrollLeft(index + 1)} className="scroll-button">◀</button>
+
+                <section className="movie-list" ref={el => scrollRefs.current[index + 1] = el}>
+                  {category.movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} onClick={handleMovieClick} />
+                  ))}
+                </section>
+
+                <button onClick={() => scrollRight(index + 1)} className="scroll-button">▶</button>
+              </div>
+            </div>
+          ))
+        )
+      }
+
       <MovieDetails movie={selectedMovie} onClose={handleCloseDetails} />
     </div>
   );
